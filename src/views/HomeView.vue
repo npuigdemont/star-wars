86615e4f040fb6beb78d15baacef3045fc327134
Nuri <72916917+npuigdemont@users.noreply.github.com>
2022-05-24 19:06:17 +0200

    exercici1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+        <button @click="saberNaus()">Obtenir naus</button><br>
+    <ul  v-for="(item, id) of naus" :key="id">
+      <li>{{item.name}}</li>
+      <li>{{item.model}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions, mapState } from 'vuex'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  methods: {
+    ...mapActions(['saberNaus']),
+  },
+  computed: {
+    ...mapState(['naus'])
   }
 }
 </script>
+
+<style scoped>
+li {
+  list-style: none;
+}
+</style>
